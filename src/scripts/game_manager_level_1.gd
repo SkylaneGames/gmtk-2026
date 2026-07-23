34f0674138ce3e1d429b23f10d2c92b92d39cbda
Jeremy Sapsed <dev@jsapsed.co.uk>
2026-07-23 13:20:43 +0100

    Add basic door

--- src/scripts/game_manager_level_1.gd
+++ src/scripts/game_manager_level_1.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var start_time_seconds: float = 60
+@export var doors: Array[Door]
+@export_range(0, 100, 1, "The % of time left in the level which will trigger the door to close.") var door_times: Array[float]
 
 var time_remaining: float
 
@@ -9,6 +11,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	time_remaining -= delta
+
+	var time_remaining_percent: float = time_remaining / start_time_seconds * 100
+	for i in door_times.size():
+		if (time_remaining_percent < door_times[i]):
+			doors[i].close()
 
 func _on_exit_player_exited() -> void:
 	print("Level Completed!")
