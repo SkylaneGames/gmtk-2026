991ec5e7b608baaa939e68a723d69e8561af02a4
Jeremy Sapsed <dev@jsapsed.co.uk>
2026-07-26 03:12:36 +0100

    MemoryPickup - Further improvements

--- src/scripts/memory_pickup.gd
+++ src/scripts/memory_pickup.gd
@@ -7,6 +7,9 @@ extends Node3D
 @export var memory_image: Texture2D # JT
 @export var memory_music: AudioStream # JT
 
+@onready var body: Node3D = $Body
+@onready var emitter: GPUParticles3D = $Body/GPUParticles3D
+
 var memory_sound: AudioStreamPlayer3D #JT
 
 var starting_y: float
@@ -14,7 +17,7 @@ var elapsed_time: float = 0.0
 var collected: bool = false
 
 func _ready() -> void:
-	starting_y = position.y
+	starting_y = body.position.y
 
 	memory_sound = AudioStreamPlayer3D.new()
 	add_child(memory_sound)
@@ -22,11 +25,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	elapsed_time += delta
 
-	rotate_y(rotation_speed * delta)
+	body.rotate_y(rotation_speed * delta)
 
-	position.y = starting_y + sin(
-		elapsed_time * bob_speed
-	) * bob_height
+	body.position.y = starting_y + sin(elapsed_time * bob_speed) * bob_height
 
 func _on_interactable_interaction_started(
 	interactor: Interactor
@@ -42,11 +43,13 @@ func _on_interactable_interaction_started(
 	var player: Player = interactor.root
 	player.pickup_memory(memory_image)
 
-	visible = false
+	emitter.emitting = false
 
 	if memory_music != null:
 		memory_sound.stream = memory_music
 		memory_sound.play()
 		await memory_sound.finished
+
+	await get_tree().create_timer(3).timeout
 
 	queue_free()
